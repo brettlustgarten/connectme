a24e64a44eb84e702ba2f2971b98541aeaaec9e0
brettlustgarten <57907599+brettlustgarten@users.noreply.github.com>
2019-11-19 15:10:35 -0600

    Add files via upload

---
+++ connectMeWebApi.py
@@ -0,0 +1,29 @@
+from flask_api import FlaskAPI
+from flask import request, jsonify
+from parse import parse_new_user_data
+app = FlaskAPI(__name__)
+
+# @app.route('/api/connectme/new_user', methods=["GET", "POST"])
+# def get_new_user_data():
+    # if request.method == "POST":
+	
+	# else:
+	
+    # return {'request data': request.data}
+	
+@app.route('/api/connectme/new_user', methods=["GET", "POST"])
+def parse_user():
+	if request.method == "POST":
+		# print(request.data['photo'])
+		if 'photo' in request.data:
+			photoData = request.data['photo']
+			print(photoData)
+			output = parse_new_user_data(photoData)
+			print(output)
+			return jsonify({"output":output})
+		else:
+			return "Error: No photo field provided."
+    # return {'request data': request.data}
+	
+if __name__ == "__main__":
+    app.run(debug=True)
--- parse.py
+++ parse.py
@@ -2,13 +2,10 @@
 from BusinessCardParser import BusinessCardParser, ContactInfo
 from optparse import OptionParser
 try:
-	from PIL import Image
+	from PIL import Image, ImageFilter
 except ImportError:
     import Image
 import pytesseract
-import glob
-import os
-import requests
 import json
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
@@ -16,56 +13,57 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\t
 headers = {
 	'Content-type': 'application/json'
 	}
-while True:
-	getResp = requests.get('http://127.0.0.1:5000/api/connectme/new_user')
-	imageDict = getResp.json()["request data"]
-	print(imageDict.keys())
-	if "photo" in imageDict.keys():
-		imageString = imageDict["photo"]
 
 
-		# Simple image to string
-		output = pytesseract.image_to_string(Image.open(imageString))
+def parse_new_user_data(imageData):
+	# Simple image to string
+	output = pytesseract.image_to_string(Image.open(imageData))
 
-		""" Extract name, number and email from text of business cards """
-		# Read in options
-		usage = 'parse.py [options] file1 file2 ...'
-		opt_parser = OptionParser()
-		opt_parser.add_option('-m', '--model', dest="model_file",
-						  help="read name classifier from MODEL_FILE")
+	""" Extract name, number and email from text of business cards """
+	# Read in options
+	usage = 'parse.py [options] file1 file2 ...'
+	opt_parser = OptionParser()
+	opt_parser.add_option('-m', '--model', dest="model_file",
+					  help="read name classifier from MODEL_FILE")
 
-		(options, args) = opt_parser.parse_args()
+	(options, args) = opt_parser.parse_args()
 
-		parser = BusinessCardParser("data/name.model")
+	parser = BusinessCardParser("data/name.model")
 
-		info = parser.getContactInfo(output)
+	info = parser.getContactInfo(output)
 
-		print('-' * 40)      
-		print(text)
-		print('-' * 40)
+	print('-' * 40)      
+	# print(output)
+	print('-' * 40)
 
-		if info.getName() is None:
-			print("Could not extract name from %s" % (file))
-		if info.getPhoneNumber() is None:
-			print("Could not extract number from %s" % (file))
-		if info.getEmailAddress() is None:
-			print("Could not extract email from %s" % (file))
+	if info.getName() is None:
+		print("Could not extract name")
+	if info.getPhoneNumber() is None:
+		print("Could not extract number")
+	if info.getEmailAddress() is None:
+		print("Could not extract email")
 
-		print("Extracted Info:")
-		print(info)
-		print(info.getName())
-		print(info.getPhoneNumber())
-		print(info.getEmailAddress())
+	# print("Extracted Info:")
+	# print(info)
+	# print(info.getName())
+	# print(info.getPhoneNumber())
+	# print(info.getEmailAddress())
 
-		new_person = {
-			"name": info.getName(),
-			"email": info.getEmailAddress(),
-			"phone_number": info.getPhoneNumber,
-			}
-
-		resp = requests.post('http://127.0.0.1:5000/api/connectme/parsed_user', json=new_person, headers=headers)
-		print(resp.text)
-
-		
+	parsedName = info.getName()
+	parsedNumber = info.getPhoneNumber()
+	parsedEmail = info.getEmailAddress()
+	
+	print(parsedName)
+	print(parsedNumber)
+	print(parsedEmail)
+	
+	new_person = {
+		"name": parsedName,
+		"email": parsedEmail,
+		"phone_number": parsedNumber,
+		}
+	
+	print(new_person)
+	return(new_person)
 
     
